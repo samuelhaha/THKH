3892fc87a2deca72a90a6d9fead9e2ab67884992
Vianiece <vianiece04@gmail.com>
2022-02-01 12:09:20 +0800

    modify supervisor

--- html/js/supervisorReport/supervisorReport.js
+++ html/js/supervisorReport/supervisorReport.js
@@ -1,25 +1,28 @@
 
 $(document).ready(function () {
-    $("#sup_form").submit(function (e) {
+    $("#sup_form").click(function (e) {
       e.preventDefault();
-      var h_sp_factors = document.getElementById("h_sp_factors").value;
-      var h_sp_recommend = document.getElementById("h_sp_recommend").value;
-      var h_sp_reportFile = $("input[name='h_sp_reportFile']").value;
-      var date = document.getElementById("date").value;
-      var time = document.getElementById("time").value; 
+      // var h_sp_factors = document.getElementById("h_sp_factors").value;
+      // var h_sp_recommend = document.getElementById("h_sp_recommend").value;
+      //var h_sp_reportFile = $("input[name='h_sp_reportFile']").value;
+      // var date = document.getElementById("date").value;
+      // var time = document.getElementById("time").value; 
+
+      //var $id =  ;  //how to pass the id?
       $.ajax({
         method: "POST",
-        url: "/THKH/laravel/api/supervisor-add",
+        url: "/THKH/laravel/api/supervisor-add/" + $id,
         headers: {Authorization: 'Bearer ' + sessionStorage.getItem("jwt")},
-        data: {
-          h_sp_factors: h_sp_factors,
-          h_sp_recommend: h_sp_recommend,
-          h_sp_reportFile: h_sp_reportFile,
-          date: date,
-          time: time
-        },
+        data: new FormData(this),
+        dataType: 'json',
+        // data: {
+        //   h_sp_factors: h_sp_factors,
+        //   h_sp_recommend: h_sp_recommend,
+        //   h_sp_reportFile: h_sp_reportFile,
+        // },
         success: function (response) {
          console.log(response);
+         $("#uploaded_image").html(response.uploaded_image);
         },
         
       })
